627fafc425623a8b9af659453b374ee1d3777ba3
max-allan <max_allan@hotmail.com>
2024-05-25 16:02:35 +0000

    Clients couldn't write to their root disk

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ dnf -y install tftp tftp-server syslinux-tftpboot syslinux dhcp-server nfs-utils
 cp dhcpd.conf /etc/dhcp/dhcpd.conf
 IF=$(nmcli -t -f NAME c show --active | grep -v ^lo$)
 IP=$(nmcli -t -f IP4.ADDRESS c show ${IF} | cut -f2 -d: | cut -d/ -f1)
+# Assumes a /24. If you don't have a subnet on a word boundary, sucks to be you
+SUBNET=$(nmcli -t -f IP4.ADDRESS c show ${IF} | cut -f2 -d: | sed -E "s+(.*)\..*\/(.*)+\1.0\/\2+")
 # Add 10.99 address for DHCP network
 IP10=10.99.99.1
 nmcli con mod ${IF} +ipv4.ADDRESS ${IP10}/24
@@ -10,8 +12,7 @@ nmcli con up ${IF}
 
 echo 'INTERFACESv4="'$IF'"' >  /etc/default/isc-dhcp-server
 
-systemctl start dhcpd
-systemctl enable dhcpd
+systemctl enable --now dhcpd
 
 sed -i s+/var/lib/tftpboot+/tftpboot+ /usr/lib/systemd/system/tftp.service
 
@@ -19,8 +20,12 @@ mkdir /tftpboot/pxelinux.cfg
 echo "default centos9
 label centos9
   kernel images/centos9/vmlinuz
-  append initrd=images/centos9/initrd.img ip=dhcp root=nfs:${IP10}:/client1 rw selinux=0
+  append initrd=images/centos9/initrd.img ip=dhcp root=nfs:${IP}:/client1 rw selinux=0
 " > /tftpboot/pxelinux.cfg/default
+
+#Post boot the client will re-request DHCP and this time get the main DHCP server, not us.
+# Hence $IP
+
 # Do some checking to get the client1's mac address and replace default with 01-mac
 # https://wiki.syslinux.org/wiki/index.php?title=PXELINUX
 
@@ -32,13 +37,12 @@ label centos9
 #   menu label ^Install system
 #   menu default
 #   kernel images/centos9/vmlinuz
-#   append initrd=images/centos9/initrd.img ip=dhcp inst.repo=http://${IP10}/centos9/BaseOS/
+#   append initrd=images/centos9/initrd.img ip=dhcp inst.repo=http://${IP}/centos9/BaseOS/
 # " > /tftpboot/pxelinux.cfg/default
 
-systemctl start tftp
-systemctl start nfs-server
-systemctl enable tftp
-systemctl enable nfs-server
+
+systemctl enable --now tftp
+systemctl enable --now nfs-server
 
 firewall-cmd --add-service=tftp
 firewall-cmd --add-service=nfs
@@ -65,7 +69,7 @@ for n in $(seq 1 5) ; do
     mkdir -p /src/client${n}/{work,root} /client${n}
     #mount -t overlay overlay -o  index=on,nfs_export=on,lowerdir=/src/root-common,upperdir=/src/client${n}/root,workdir=/src/client${n}/work /client${n}
     echo "overlay /client${n} overlay  index=on,nfs_export=on,lowerdir=/src/root-common,upperdir=/src/client${n}/root,workdir=/src/client${n}/work 0 0" >> /etc/fstab
-    echo "/client${n} 10.99.99.0/24(sync)" >> /etc/exports
+    echo "/client${n} 1${SUBNET}(rw,no_root_squash)" >> /etc/exports
 done
 mount -a
 exportfs -a
